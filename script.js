1b7fafcc39cdd32bdec7ad1d4fa56c93f8bd8187
Code <119617297+Vpan23@users.noreply.github.com>
2023-09-13 23:21:50 -0500

    se anadieron nuevos avances.

--- script.js
+++ script.js
@@ -1,38 +1,78 @@
-const searchButton = document.getElementById('search');
+const searchButton = document.getElementById("search");
 let validation = true;
-const searchText = document.querySelector('.search-text');
+const searchText = document.querySelector(".search-text");
 
-searchButton.addEventListener('click', function() {
- 
-    if(validation === true) {
-        searchButton.style.transform = 'translateX(-120px)';
-        
-        validation = false;
+searchButton.addEventListener("click", function () {
+  if (validation === true) {
+    searchButton.style.transform = "translateX(-120px)";
 
-        setTimeout(() => {
-            searchText.style.display = 'flex';
-        }, 400);
-    }else {
-        searchButton.style.transform = 'translateX(0)';
-        validation = true;
-        searchText.style.display = 'none';
-    }
-    
-})
+    validation = false;
+
+    setTimeout(() => {
+      searchText.style.display = "flex";
+    }, 400);
+  } else {
+    searchButton.style.transform = "translateX(0)";
+    validation = true;
+    searchText.style.display = "none";
+  }
+});
 
 // seccion de juego
-const TIENDA = document.querySelector('.tienda');
-const CASA = document.querySelector('.casa');
-const BATALLA = document.querySelector('.battle');
+const TIENDA = document.querySelector(".tienda");
+const CASA = document.querySelector(".casa");
+const BATALLA = document.querySelector(".battle");
 
-TIENDA.addEventListener('click', () => {
-    console.log('tienda');
-})
+TIENDA.addEventListener("click", () => {
+  console.log("tienda");
+  barraDeCarga();
+  setTimeout( function(){
+    tienda();
+  }, 4000)
+});
 
-CASA.addEventListener('click', () => {
-    console.log('casa');
-})
+CASA.addEventListener("click", () => {
+  console.log("casa");
+});
 
-BATALLA.addEventListener('click', () => {
-    console.log('batalla');
-})
+BATALLA.addEventListener("click", () => {
+  console.log("batalla");
+});
+
+const BarraDeCarga = document.querySelector(".barraDeCarga");
+const cargandoText = document.querySelector(".cargando");
+const monedasTextDisplay = document.getElementById('monedas');
+
+let monedas = 0; 
+monedasTextDisplay.innerHTML = monedas;
+
+// Barra de carga con Setinterval.
+function barraDeCarga() {
+  // Esta barra mostrar una enfrente de la pantalla de juego una barra que se estara cargando.
+  BarraDeCarga.style.display = 'flex';
+  cargandoText.innerHTML = "CARGANDO";
+    function miFuncion() {
+        cargandoText.innerHTML += ".";
+    }
+      var intervaloID = setInterval(miFuncion, 1000);
+      setTimeout(function() {
+        clearInterval(intervaloID);
+        BarraDeCarga.style.display = 'none';
+      }, 4000);
+}
+
+// Activador de la Funcion de Tienda.
+const tiendaDisplay = document.querySelector('.tiendaDisplay');
+function tienda() {
+    tiendaDisplay.style.display = 'flex';
+    bienvenido();
+    
+}
+
+const bienvenidoTextDisplay = document.querySelector('.bienvenido');
+function bienvenido() {
+    bienvenidoTextDisplay.innerHTML = 'Has entrado a la Tienda';
+    setTimeout(() => {
+        bienvenidoTextDisplay.innerHTML = ''
+    }, 1200);
+}
